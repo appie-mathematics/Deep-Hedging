a6e524415a1a34b5185b86c6efa67da1a302b214
Albin Jaldevik <albja664@student.liu.se>
2023-05-31 16:15:15 +0200

    costs

---
+++ src/Costs.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+
+class CostFunction(ABC, callable):
+
+    @abstractmethod
+    def cost(self, action, state = None):
+        pass
+
+    def __call__(self, action, state = None):
+        return self.cost(action, state)
+
+
+
+
+class PorportionalCost(CostFunction):
+
+    def __init__(self, cost_rate):
+        self.cost_rate = cost_rate
+
+    def cost(self, action, state = None):
+        # state = hedge_paths[:,:t], cash_account[:,:t], positions[:,:t], portfolio_value[:,:t]
+        purchase_price = state[0][:, -1, :] * action
+        return self.cost_rate * action.abs().sum(dim=-1, keepdim=True)
