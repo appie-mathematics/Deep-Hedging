df9fd194166c96e7e8d24d83339d05669e3cdaf7
Albin Jaldevik <albja664@student.liu.se>
2023-05-31 17:14:41 +0200

    CVaR

--- src/RiskMeasures.py
+++ src/RiskMeasures.py
@@ -11,3 +11,17 @@ class RiskMeasure(ABC, torch.nn.Module):
 
 
 
+
+
+
+class CVaR(RiskMeasure):
+    def __init__(self, alpha):
+        super().__init__()
+        self.alpha = alpha
+
+    def __call__(self, portfolio_value: torch.Tensor):
+        # portifolio_value: P x 1 (final portfolio value for every path)
+        # return: 1 x 1
+        # return expected shortfall
+        # TODO: check if this is correct
+        return portfolio_value.mean() + (1 / (1 - self.alpha)) * portfolio_value[portfolio_value < portfolio_value.quantile(self.alpha)].mean()
