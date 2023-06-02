4032e7717e99a5e8da85edd4d6b9297ee050f092
Albin Jaldevik <albja664@student.liu.se>
2023-06-02 12:17:41 +0200

    move strike to object property

--- src/instruments/Derivatives.py
+++ src/instruments/Derivatives.py
@@ -33,18 +33,18 @@ class EuropeanCall(EuropeanOption):
 
 class BSCall(EuropeanCall, Instrument):
 
-    def value(self, primary_path, strike, expiry, drift, volatility) -> torch.Tensor:
+    def value(self, primary_path, expiry, drift, volatility) -> torch.Tensor:
         expiries = expiry - torch.arange(primary_path.shape[1]) * (expiry / primary_path.shape[1])
 
-        d1 = (torch.log(primary_path/strike) + (drift + 0.5 * volatility**2) * expiries) / (volatility * torch.sqrt(expiries))
+        d1 = (torch.log(primary_path/self.strike) + (drift + 0.5 * volatility**2) * expiries) / (volatility * torch.sqrt(expiries))
 
         d2 = d1 - volatility * torch.sqrt(expiries)
 
-        return primary_path * Normal(0, 1).cdf(d1) - strike * torch.exp(-drift * expiries) * Normal(0, 1).cdf(d2)
+        return primary_path * Normal(0, 1).cdf(d1) - self.strike * torch.exp(-drift * expiries) * Normal(0, 1).cdf(d2)
 
-    def delta(self, primary_path, strike, expiry, drift, volatility) -> torch.Tensor:
+    def delta(self, primary_path, expiry, drift, volatility) -> torch.Tensor:
         expiries = expiry - torch.arange(primary_path.shape[1]) * (expiry / primary_path.shape[1])
 
-        d1 = (torch.log(primary_path / strike) + (drift + 0.5 * volatility ** 2) * expiries) / (volatility * torch.sqrt(expiries))
+        d1 = (torch.log(primary_path / self.strike) + (drift + 0.5 * volatility ** 2) * expiries) / (volatility * torch.sqrt(expiries))
 
         return Normal(0, 1).cdf(d1)
