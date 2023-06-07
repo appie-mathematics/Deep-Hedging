4581e053cdde24c10b4b9603cf60b4b661ee9dc7
Albin Jaldevik <albja664@student.liu.se>
2023-06-07 23:40:07 +0200

    update demo

--- src/demo.py
+++ src/demo.py
@@ -17,25 +17,31 @@ from ExperimentRunner import ExperimentRunner, SimpleRunner, plot_dists
 seed = 1337
 torch.manual_seed(seed)
 
-T = 10
+T = 10 # Number of time steps
 total_rate = 0.0
 step_interest_rate = (total_rate + 1) ** (1 / T) - 1
+
+# Define an underlying stock
 drift = step_interest_rate
 volatility = 0.2
 S0 = 1
 stock = GeometricBrownianStock(S0, drift, volatility)
+
+# Define the claim, hedging instruments, criterion, and costs for the experiment
 contingent_claim: Claim = BSCall(stock, S0, T, drift, volatility)
 hedging_instruments: List[Instrument] = [stock]
 criterion: torch.nn.Module = RiskMeasures.WorstCase()
-prop_cost = 0.0
-cost_function: CostFunction = PorportionalCost(prop_cost)
+cost_function: CostFunction = PorportionalCost(0.0)
 
+# Define the agent
 epochs = 50
 h_dim = 15
 paths = int(1e4)
 verbose = False
-
 runner = ExperimentRunner("recurrent", pref_gpu=True)
+
+# Run the experiment
 runner.run(contingent_claim, hedging_instruments, criterion, T, step_interest_rate, epochs, paths, verbose, cost_function, h_dim)
 
+# Plot the results
 runner.plot_runner(animate=True, save=False, n=3)
