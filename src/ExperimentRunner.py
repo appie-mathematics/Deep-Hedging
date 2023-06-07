6b41a18b1ff8330720c948c11680acd1328a69e0
Albin Jaldevik <albja664@student.liu.se>
2023-06-07 19:09:32 +0200

    bugfix

--- src/ExperimentRunner.py
+++ src/ExperimentRunner.py
@@ -245,7 +245,7 @@ def plot_dists(runners: List[ExperimentRunner], save=False, file_prefix='plot', 
         val_loss = runner.validation_logs["validation_loss"]
         plot = sns.histplot((val_profit+price).numpy(), stat='count', kde=False, label=f'{runner.agent_type}, N: {len(val_profit)}, Loss: {val_loss:.2f}', binwidth=0.03, alpha=0.5)
     plot.set_title("P&L Distribution")
-    plot.set_xlim(x_lim, x_lim)
+    plot.set_xlim(-x_lim, x_lim)
     plot.grid()
     plot.legend()
     if save:
